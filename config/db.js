f13207de3ac489047b568abf7d0c964307ab9ebb
Sneha Datta <snehadatta431@gmail.com>
2026-06-06 16:42:12 +0530

    Configure Railway MySQL

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ import { createConnection } from 'mysql2';
 
 const connection = createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
