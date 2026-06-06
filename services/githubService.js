860e819b97f846c50002419eb072f5b9883ba2b8
Sneha Datta <snehadatta431@gmail.com>
2026-06-06 16:55:10 +0530

    add header to github service

--- services/githubService.js
+++ services/githubService.js
@@ -3,7 +3,14 @@ import axios from "axios";
 export const getGithubProfile = async (username) => {
 
     const response = await axios.get(
-        `https://api.github.com/users/${username}`
+        `https://api.github.com/users/${username}`,
+        {
+            headers: {
+                "User-Agent": "github-profile-analyzer",
+                "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`,
+                "Accept": "application/vnd.github+json"
+            }
+        }
     );
 
     return response.data;
